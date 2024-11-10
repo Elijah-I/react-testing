09bec8f36c2c24e00ca13b722df144189779cee9
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2024-11-10 14:26:04 +0300

    ref: simplify ProductImageGallery test

--- tests/components/ProductImageGallery.test.tsx
+++ tests/components/ProductImageGallery.test.tsx
@@ -1,19 +1,27 @@
 import ProductImageGallery from "@/components/ProductImageGallery";
 import { emptyImageUrls, imageUrls } from "../mocks/ProductImageGallery.mock";
+import { ComponentProps } from "react";
 
 describe("ProductImageGallery", () => {
+  const renderComponent = (
+    props: ComponentProps<typeof ProductImageGallery>
+  ) => {
+    const { container } = render(<ProductImageGallery {...props} />);
+
+    return {
+      container,
+      galleryItems: screen.queryAllByRole("img")
+    };
+  };
+
   it("should return <null> when <imageUrls> is empty", () => {
-    const { container } = render(
-      <ProductImageGallery imageUrls={emptyImageUrls} />
-    );
+    const { container } = renderComponent({ imageUrls: emptyImageUrls });
 
     expect(container).toBeEmptyDOMElement();
   });
 
   it("should render correct <images> when <imageUrls> is not empty", () => {
-    render(<ProductImageGallery imageUrls={imageUrls} />);
-
-    const galleryItems = screen.getAllByRole("img");
+    const { galleryItems } = renderComponent({ imageUrls });
 
     expect(galleryItems).toHaveLength(imageUrls.length);
 
