50f76b2f56c41cfc2788b602a349ea76e0321f55
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2024-11-10 14:33:55 +0300

    ref: simplify UserAccount test

--- tests/components/UserAccount.test.tsx
+++ tests/components/UserAccount.test.tsx
@@ -1,34 +1,28 @@
 import UserAccount from "@/components/UserAccount";
+import { ComponentProps } from "react";
 import { admin, editButtonText, user } from "../mocks/UserAccount.mock";
 
 describe("UserAccount", () => {
-  it("should render <user.name> if <!user.isAdmin>", () => {
-    render(<UserAccount user={user} />);
-    expect(screen.getByText(user.name)).toBeInTheDocument();
+  const renderComponent = (props: ComponentProps<typeof UserAccount>) => {
+    render(<UserAccount {...props} />);
+
+    return {
+      userName: screen.getByText(props.user.name),
+      editButton: screen.queryByRole("button", { name: editButtonText })
+    };
+  };
+
+  it("should render <user.name> without <edit button> if <!user.isAdmin>", () => {
+    const { userName, editButton } = renderComponent({ user });
+
+    expect(userName).toBeInTheDocument();
+    expect(editButton).not.toBeInTheDocument();
   });
-});
 
-describe("UserAccount", () => {
-  it("should not render <edit button> if <!user.isAdmin>", () => {
-    render(<UserAccount user={user} />);
-    expect(screen.queryByRole("button")).not.toBeInTheDocument();
-  });
-});
+  it("should render <user.name> with <edit button> if <user.isAdmin>", () => {
+    const { userName, editButton } = renderComponent({ user: admin });
 
-describe("UserAccount", () => {
-  it("should render <user.name> if <user.isAdmin>", () => {
-    render(<UserAccount user={admin} />);
-    expect(screen.getByText(admin.name)).toBeInTheDocument();
-  });
-});
-
-describe("UserAccount", () => {
-  it("should render <edit button> if <user.isAdmin>", () => {
-    render(<UserAccount user={admin} />);
-
-    const editButton = screen.getByRole("button");
-
+    expect(userName).toBeInTheDocument();
     expect(editButton).toBeInTheDocument();
-    expect(editButton).toHaveTextContent(editButtonText);
   });
 });
