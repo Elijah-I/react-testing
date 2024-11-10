1b1e216c35ff1afe1946fc3fcd495299a7d04afa
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2024-11-10 14:28:18 +0300

    ref: simplify TermsAndConditions test

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -2,55 +2,50 @@ import TermsAndConditions from "@/components/TermsAndConditions";
 import { headerText } from "../mocks/TermsAndConditions.mock";
 
 describe("TermsAndConditions", () => {
-  it("renders correct text", () => {
+  const renderComponent = () => {
     render(<TermsAndConditions />);
 
-    const header = screen.getByRole("heading");
+    return {
+      header: screen.getByRole("heading"),
+      submitButton: screen.getByRole("button"),
+      checkbox: screen.getByRole("checkbox")
+    };
+  };
 
-    expect(header).toBeInTheDocument();
+  it("renders correct text", () => {
+    const { header } = renderComponent();
+
     expect(header).toHaveTextContent(headerText);
   });
 
   it("renders disabled <submit> button at initial state", () => {
-    render(<TermsAndConditions />);
+    const { submitButton } = renderComponent();
 
-    const submitButton = screen.getByRole("button");
-
-    expect(submitButton).toBeInTheDocument();
     expect(submitButton).toBeDisabled();
   });
 
   it("renders unchecked <checkbox> at initial state", () => {
-    render(<TermsAndConditions />);
+    const { checkbox } = renderComponent();
 
-    const checkbox = screen.getByRole("checkbox");
-
-    expect(checkbox).toBeInTheDocument();
     expect(checkbox).not.toBeChecked();
   });
 
   it("renders enabled <submit> button if <agree checkbox> is checked", async () => {
-    render(<TermsAndConditions />);
+    const { checkbox, submitButton } = renderComponent();
 
-    const checkbox = screen.getByRole("checkbox");
     await userEvent.click(checkbox);
 
     expect(checkbox).toBeChecked();
-    expect(screen.getByRole("button")).toBeEnabled();
+    expect(submitButton).toBeEnabled();
   });
 
   it("renders disabled <submit> button after <agree checkbox> was checked and then unchecked", async () => {
-    render(<TermsAndConditions />);
+    const { checkbox, submitButton } = renderComponent();
 
-    const checkbox = screen.getByRole("checkbox");
     await userEvent.click(checkbox);
-
-    expect(checkbox).toBeChecked();
-    expect(screen.getByRole("button")).toBeEnabled();
-
     await userEvent.click(checkbox);
 
     expect(checkbox).not.toBeChecked();
-    expect(screen.getByRole("button")).toBeDisabled();
+    expect(submitButton).toBeDisabled();
   });
 });
