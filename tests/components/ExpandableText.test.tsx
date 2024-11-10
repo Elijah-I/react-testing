648766ee29615904a688f8f7eb3851492b1e6693
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2024-11-10 14:13:37 +0300

    ref: simplify ExpandableText test

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -7,50 +7,46 @@ import {
   textLimit,
   truncatedText
 } from "../mocks/ExpandableText.mock";
+import type { ComponentProps } from "react";
 
 describe("ExpandableText", () => {
-  it(`should render full <text> without <expand button> if the text is shorter than <limit (${textLimit})>`, () => {
-    render(<ExpandableText text={shortText} />);
+  const renderComponent = (props: ComponentProps<typeof ExpandableText>) => {
+    render(<ExpandableText {...props} />);
 
-    const articleText = screen.getByRole("article");
-    const expandButton = screen.queryByRole("button");
+    return {
+      articleText: screen.getByRole("article"),
+      expandButton: screen.queryByRole("button")
+    };
+  };
+
+  it(`should render full <text> without <expand button> if the text is shorter than <limit (${textLimit})>`, () => {
+    const { articleText, expandButton } = renderComponent({ text: shortText });
 
     expect(articleText).toHaveTextContent(shortText);
     expect(expandButton).not.toBeInTheDocument();
   });
 
   it(`should render truncated <text> and <expand button> if the text is longer than <limit (${textLimit})>`, () => {
-    render(<ExpandableText text={longText} />);
-
-    const articleText = screen.getByRole("article");
-    const expandButton = screen.getByRole("button");
+    const { articleText, expandButton } = renderComponent({ text: longText });
 
     expect(articleText).toHaveTextContent(truncatedText);
     expect(expandButton).toHaveTextContent(showMoreText);
   });
 
   it(`should render full <text> and change <expand button> label after <expand button> is clicked`, async () => {
-    render(<ExpandableText text={longText} />);
+    const { articleText, expandButton } = renderComponent({ text: longText });
 
-    const articleText = screen.getByRole("article");
-
-    const expandButton = screen.getByRole("button");
-    await userEvent.click(expandButton);
+    expandButton && (await userEvent.click(expandButton));
 
     expect(expandButton).toHaveTextContent(showLessText);
     expect(articleText).toHaveTextContent(longText);
   });
 
   it(`should render truncated <text> and <expand button> if the <collapse button> is clicked`, async () => {
-    render(<ExpandableText text={longText} />);
+    const { articleText, expandButton } = renderComponent({ text: longText });
 
-    const articleText = screen.getByRole("article");
-
-    const expandButton = screen.getByRole("button", { name: showMoreText });
-    await userEvent.click(expandButton);
-
-    const collapseButton = screen.getByRole("button", { name: showLessText });
-    await userEvent.click(collapseButton);
+    expandButton && (await userEvent.click(expandButton));
+    expandButton && (await userEvent.click(expandButton));
 
     expect(articleText).toHaveTextContent(truncatedText);
     expect(expandButton).toHaveTextContent(showMoreText);
