11c2ca5ca4aaedd3feb72349d9f0dd7bbfeb4cca
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2024-11-19 10:21:28 +0300

    feat: matchMedia global

--- tests/setupFiles/global.testing.library.ts
+++ tests/setupFiles/global.testing.library.ts
@@ -11,6 +11,22 @@ beforeAll(() => {
   global.screen = screen;
   // @ts-expect-error type
   global.userEvent = userEvent;
+
+  global.matchMedia =
+    global.matchMedia ||
+    (() => ({
+      writable: true,
+      value: vi.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(),
+        removeListener: vi.fn(),
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn()
+      }))
+    }));
 });
 
 afterAll(() => {
