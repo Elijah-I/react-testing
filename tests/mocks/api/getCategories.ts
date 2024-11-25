3980e85a14cd507621a44219cd547e61fc3430f0
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2024-11-25 15:57:08 +0300

    ref: remove getCategories

--- tests/mocks/api/getCategories.ts
+++
@@ -1,8 +0,0 @@
-import { Category } from "@/entities";
-import { createGetRequest } from "./createGetRequest";
-import { categoryList } from "../db";
-
-export const getCategories = createGetRequest<Category[]>({
-  url: "/categories",
-  response: categoryList
-});
--- tests/mocks/api/server.ts
+++ tests/mocks/api/server.ts
@@ -1,6 +1,5 @@
 import { setupServer } from "msw/node";
-import { getCategories } from "./getCategories";
 import { getProducts } from "./getProducts";
 import { getProduct } from "./getProduct";
 
-export const server = setupServer(getCategories, getProducts, getProduct);
+export const server = setupServer(getProducts, getProduct);
